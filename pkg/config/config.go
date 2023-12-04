9e5620aa35c33ff8b18656b9d0de26a20e677059
Matt Tey <stand.by.matt@gmail.com>
2023-12-04 12:40:15 +0800

    logging for config.go

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -11,9 +11,15 @@ func init() {
 	// Load environment variables from .env file
 	// if running locally
 	if os.Getenv("DOCKER_ENV_SET") != "true" {
+		log.Println("Non-docker environment detected, loading .env file")
+
 		err := godotenv.Load()
 		if err != nil {
 			log.Fatal("Error loading .env file")
 		}
+
+		log.Println("Environment variables loaded from .env file")
+	} else {
+		log.Println("Docker environment detected, skipping .env file load")
 	}
 }
