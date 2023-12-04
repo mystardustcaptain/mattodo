7ee682c0f2708b22982b5613e40eea3fbd5c70c9
Matt Tey <stand.by.matt@gmail.com>
2023-12-04 09:28:52 +0800

    database update
    - change fields to NOT NULL
    - generated more dummy datas

--- pkg/db/queries.sql
+++ pkg/db/queries.sql
@@ -1,19 +1,19 @@
 --- tables used in the database
 
-CREATE TABLE users (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    oauth_provider TEXT,
-    oauth_id TEXT,
-    name TEXT,
-    email TEXT
-);
-
 CREATE TABLE todos (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    user_id INTEGER,
-    title TEXT,
-    completed BOOLEAN,
-    created_at TIMESTAMP,
-    updated_at TIMESTAMP,
+    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    user_id INTEGER NOT NULL,
+    title TEXT NOT NULL,
+    completed BOOLEAN NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL,
     FOREIGN KEY (user_id) REFERENCES users(id)
-);
+)
+
+CREATE TABLE users (
+    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    oauth_provider TEXT NOT NULL,
+    oauth_id TEXT NOT NULL,
+    name TEXT NOT NULL,
+    email TEXT NOT NULL
+)
