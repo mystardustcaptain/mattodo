a5d7ae3c1e64e1d8a00b7a354600bd8d55255730
Matt Tey <stand.by.matt@gmail.com>
2023-12-04 09:31:46 +0800

    todo functionalities
    complete functionalities developed in both controller and in model

--- pkg/controller/controllerTodo.go
+++ pkg/controller/controllerTodo.go
@@ -1,8 +1,11 @@
 package controller
 
 import (
+	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
+	"strconv"
 
 	"github.com/gorilla/mux"
 	"github.com/mystardustcaptain/mattodo/pkg/auth"
@@ -10,16 +13,16 @@ import (
 )
 
 func (c *Controller) GetTodos(w http.ResponseWriter, r *http.Request) {
-	// Retrieve userID from the request context
-	userID, ok := r.Context().Value("userID").(int)
+	// Retrieve iam from the request context
+	iam, ok := r.Context().Value("userID").(int)
 	if !ok {
-		fmt.Println("userID: ", userID)
+		fmt.Println("userID: ", iam)
 		fmt.Println("Failed to read context")
 		respondWithError(w, http.StatusInternalServerError, "Failed to read context")
 		return
 	}
 
-	todoItems, err := model.GetAllTodoItems(c.Database, userID)
+	todoItems, err := model.GetAllTodoItems(c.Database, iam)
 	if err != nil {
 		fmt.Println("Failed to get todo items: ", err.Error())
 		respondWithError(w, http.StatusInternalServerError, err.Error())
@@ -30,15 +33,95 @@ func (c *Controller) GetTodos(w http.ResponseWriter, r *http.Request) {
 }
 
 func (c *Controller) CreateTodo(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Create Todo")
+	// Retrieve iam from the request context
+	iam, ok := r.Context().Value("userID").(int)
+	if !ok {
+		fmt.Println("userID: ", iam)
+		fmt.Println("Failed to read context")
+		respondWithError(w, http.StatusInternalServerError, "Failed to read context")
+		return
+	}
+
+	var t model.TodoItem
+	reqBody, _ := io.ReadAll(r.Body)
+	json.Unmarshal(reqBody, &t)
+
+	err := t.CreateTodoItem(c.Database, iam)
+	if err != nil {
+		fmt.Println("Failed to create todo item: ", err.Error())
+		respondWithError(w, http.StatusInternalServerError, "Failed to create todo item: "+err.Error())
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, t)
 }
 
 func (c *Controller) DeleteTodoById(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Delete Todo By Id")
+	// Retrieve iam from the request context
+	iam, ok := r.Context().Value("userID").(int)
+	if !ok {
+		fmt.Println("Failed to read context")
+		respondWithError(w, http.StatusInternalServerError, "Failed to read context")
+		return
+	}
+
+	t := model.TodoItem{UserID: iam}
+
+	// Retrieve the todo item id from the request path
+	vars := mux.Vars(r)
+	todoItemID, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid todo ID")
+		return
+	}
+	t.ID = todoItemID
+
+	err = t.DeleteTodoItem(c.Database, iam)
+	if err != nil {
+		fmt.Println("Failed to delete todo item: ", err.Error())
+		respondWithError(w, http.StatusInternalServerError, "Failed to delete todo item: "+err.Error())
+		return
+	}
+
+	// Operation was successful, but no content to return
+	respondWithJSON(w, http.StatusNoContent, nil)
 }
 
 func (c *Controller) MarkTodoCompleteById(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Mark Todo to Done By Id")
+	// Retrieve iam from the request context
+	iam, ok := r.Context().Value("userID").(int)
+	if !ok {
+		fmt.Println("Failed to read context")
+		respondWithError(w, http.StatusInternalServerError, "Failed to read context")
+		return
+	}
+
+	t := model.TodoItem{UserID: iam}
+
+	// Retrieve the todo item id from the request path
+	vars := mux.Vars(r)
+	todoItemID, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid todo ID")
+		return
+	}
+	t.ID = todoItemID
+
+	err = t.MarkComplete(c.Database, iam)
+	if err != nil {
+		fmt.Println("Failed to mark complete todo item: ", err.Error())
+		respondWithError(w, http.StatusInternalServerError, "Failed to mark complete todo item: "+err.Error())
+		return
+	}
+
+	err = t.GetTodoItem(c.Database, iam)
+	if err != nil {
+		fmt.Println("Failed to retrieve item after mark complete: ", err.Error())
+		respondWithError(w, http.StatusInternalServerError, "Failed to retrieve item after mark complete: "+err.Error())
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, t)
 }
 
 func (c *Controller) RegisterTodoRoutes(router *mux.Router) {
--- pkg/model/todo.go
+++ pkg/model/todo.go
@@ -15,17 +15,7 @@ type TodoItem struct {
 	UpdatedAt time.Time `json:"updated_at"`
 }
 
-// CreateTodoItem function to easily create a new TodoItem for a User.
-func NewTodoItem(userID int, title string) *TodoItem {
-	return &TodoItem{
-		UserID:    userID,
-		Title:     title,
-		Completed: false,
-		CreatedAt: time.Now(),
-		UpdatedAt: time.Now(),
-	}
-}
-
+// GetAllTodoItems function to get all TodoItems for a User of a given userID.
 func GetAllTodoItems(db *sql.DB, userID int) ([]*TodoItem, error) {
 	var todoItems []*TodoItem
 
@@ -37,6 +27,7 @@ func GetAllTodoItems(db *sql.DB, userID int) ([]*TodoItem, error) {
 	}
 	defer rows.Close()
 
+	// Iterate over the rows
 	for rows.Next() {
 		var todoItem TodoItem
 		err := rows.Scan(&todoItem.ID, &todoItem.UserID, &todoItem.Title, &todoItem.Completed, &todoItem.CreatedAt, &todoItem.UpdatedAt)
@@ -45,6 +36,8 @@ func GetAllTodoItems(db *sql.DB, userID int) ([]*TodoItem, error) {
 		}
 		todoItems = append(todoItems, &todoItem)
 	}
+
+	// Check for errors after we are done iterating over the rows
 	if err = rows.Err(); err != nil {
 		return nil, err
 	}
@@ -52,14 +45,107 @@ func GetAllTodoItems(db *sql.DB, userID int) ([]*TodoItem, error) {
 	return todoItems, nil
 }
 
-// MarkComplete function to mark a TodoItem as completed.
-func (t *TodoItem) MarkComplete() {
-	t.Completed = true
+// CreateTodoItem function to create a new TodoItem in the database.
+// Takes in a userID to ensure that the TodoItem belongs to the User.
+func (t *TodoItem) CreateTodoItem(db *sql.DB, userID int) error {
+	query := "INSERT INTO todos (user_id, title, completed, created_at, updated_at) VALUES (?, ?, ?, ?, ?)"
+
+	// You can only create a todo item for yourself
+	// ? Should we return an error if the user tries to create a todo item for someone else?
+	// ? Or should we just ignore the userID in the request body?
+	// ? Or should we just return an error if the userID in the request body is not the same as the userID in the request context?
+	// Simple approach for now
+	t.UserID = userID
+	t.CreatedAt = time.Now()
 	t.UpdatedAt = time.Now()
+
+	result, err := db.Exec(query, t.UserID, t.Title, t.Completed, t.CreatedAt, t.UpdatedAt)
+	if err != nil {
+		return err
+	}
+
+	// Get the ID of the newly created TodoItem
+	todoItemID, err := result.LastInsertId()
+	if err != nil {
+		return err
+	}
+
+	// Set the ID of the TodoItem to the receiver
+	t.ID = int(todoItemID)
+
+	return nil
 }
 
-// UpdateTitle function to update the title of a TodoItem.
-func (t *TodoItem) UpdateTitle(newTitle string) {
-	t.Title = newTitle
+// MarkComplete function to mark a TodoItem as completed.
+// Takes in a userID to ensure that the TodoItem belongs to the User.
+func (t *TodoItem) MarkComplete(db *sql.DB, userID int) error {
+	query := "UPDATE todos SET completed = ?, updated_at = ? WHERE id = ? AND user_id = ?"
+
+	// Update the TodoItem
+	// Mark it as completed and update the timestamp
+	t.Completed = true
 	t.UpdatedAt = time.Now()
+
+	result, err := db.Exec(query, t.Completed, t.UpdatedAt, t.ID, userID)
+	if err != nil {
+		// Reset the TodoItem to its original state
+		t.Completed = false
+		t.UpdatedAt = time.Time{}
+		return err
+	}
+
+	// Check if the TodoItem was actually updated
+	rowsAffected, err := result.RowsAffected()
+	if err != nil {
+		return err
+	}
+
+	// If no rows were affected, then the TodoItem was not found
+	// or it does not belong to the user
+	// or it was already deleted
+	if rowsAffected == 0 {
+		return sql.ErrNoRows
+	}
+
+	return nil
+}
+
+// GetTodoItem function to get a TodoItem by its ID for a User of a given userID.
+func (t *TodoItem) GetTodoItem(db *sql.DB, userID int) error {
+	query := "SELECT id, user_id, title, completed, created_at, updated_at FROM todos WHERE id = ? AND user_id = ?"
+
+	err := db.QueryRow(query, t.ID, userID).Scan(&t.ID, &t.UserID, &t.Title, &t.Completed, &t.CreatedAt, &t.UpdatedAt)
+	if err != nil {
+		// Error returned might not be clear enough to the user
+		// whether the TodoItem was not found (does not exist or does not belong to the user)
+		// or if there was some other error
+		return err
+	}
+
+	return nil
+}
+
+// DeleteTodoItem function to delete a TodoItem by its ID for a User of a given userID.
+func (t *TodoItem) DeleteTodoItem(db *sql.DB, userID int) error {
+	query := "DELETE FROM todos WHERE id = ? AND user_id = ?"
+
+	result, err := db.Exec(query, t.ID, userID)
+	if err != nil {
+		return err
+	}
+
+	// Check if the TodoItem was actually deleted
+	rowsAffected, err := result.RowsAffected()
+	if err != nil {
+		return err
+	}
+
+	// If no rows were affected, then the TodoItem was not found
+	// or it does not belong to the user
+	// or it was already deleted
+	if rowsAffected == 0 {
+		return sql.ErrNoRows
+	}
+
+	return nil
 }
